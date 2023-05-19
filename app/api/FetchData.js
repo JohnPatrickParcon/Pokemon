56c56dda0827a7561503f6bda43d4e312c29e3a6
JohnPatrickParcon <johnpatrickparcon@gmail.com>
2023-05-19 09:19:37 +0800

    -search function added -Todo: show searched pokemon

--- app/api/FetchData.js
+++ app/api/FetchData.js
@@ -1,8 +1,16 @@
 import axios from "axios";
 
-export const ListURL ="https://pokeapi.co/api/v2/pokemon/?limit=20&offset=0"
+export const ListURL ="https://pokeapi.co/api/v2/pokemon/"
 
 export async function getPokemonData(url) {
     const response = await axios.get(url)
     return response.data;
+}
+
+export async function getSearchedPokemon(searchedPokemon) {
+    if(searchedPokemon != null){
+    const response = await axios.get(ListURL + `${searchedPokemon}`)
+    return response.data;
+    }
+    return null
 }
--- app/layoutComponents/Nav.js
+++ app/layoutComponents/Nav.js
@@ -1,11 +1,24 @@
 import Link from "next/link"
+import { useState } from "react"
+import useDebounce from "./useDebounce";
+import { useQuery } from "react-query";
+import { getSearchedPokemon } from "../api/FetchData";
 
 const Nav = () => {
+  const [search, setSearch] = useState('');
+  const debouncedSearch = useDebounce(search, 1000) || null
+  const {data, error, isloading} = useQuery(['searchPokemon', debouncedSearch], () => getSearchedPokemon(debouncedSearch));
+
+  const searchHandler = (e) => {setSearch(e.target.value)};
   return (
       <nav>
           <Link href={'./'}>Home</Link>
           <div>
-            <input type="search" placeholder="Pokemon name"></input>
+            <input 
+              type="search" 
+              placeholder="Pokemon name"
+              value={search}
+              onChange={searchHandler}></input>
             <select type="text"></select>
           </div>
       </nav>
